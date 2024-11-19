9949195b761219987d7772bf8e528cfb75c1daaf
Susha000 <kseniagorban8@gmail.com>
2024-11-20 00:46:19 +0600

    Add files via upload

---
+++ new-folder/Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+class Home:
+    def __init__(self):
+        pass
+
+    def app(self):
+        st.write("Home")
---
+++ new-folder/Project2.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+class Project2:
+    def __init__(self):
+        pass
+
+    def app(self):
+        st.write("Project 2")
